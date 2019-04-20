6d100b0e43d0f27f69977e38c577c0bb9e5e7b08
Hrithik <hrithik757@gmail.com>
2019-04-20 14:48:53 +0100

    recaptcha add, portfolio and dbooks update

--- public/script/site.js
+++ public/script/site.js
@@ -8,6 +8,18 @@ $(window).scroll(function(){
 	} 
 	
 });
-
-
+$(document).ready(function() {
+  $('#comment_form').submit(function() {
+	$(this).ajaxSubmit({
+	  error: function(xhr) {
+		status('Error: ' + xhr.status);
+	  },
+	 success: function(response) {
+	  console.log(response);
+	 }
+	});
+	//Very important line, it disable the page refresh.
+	return false;
+  });
+});
 
--- routes/index.js
+++ routes/index.js
@@ -1,5 +1,6 @@
 const express = require('express'),
       sgMail = require('@sendgrid/mail')
+      request = require('request');
       
 sgMail.setApiKey(process.env.SENDGRID_API_KEY);
 
@@ -23,22 +24,40 @@ router.get("/contact", (req, res) => {
     res.render("contact");
 })
 
-router.post("/contact", async (req, res) =>{
-    
-    try{
+router.post("/contact", (req, res) =>{
+    // g-recaptcha-response is the key that browser will generate upon form submit.
+    // if its blank or null means user has not selected the captcha, so return the error.
+    if(req.body['g-recaptcha-response'] === undefined || req.body['g-recaptcha-response'] === '' || req.body['g-recaptcha-response'] === null) {
+        req.flash('error', "Please select captcha");
+        return res.redirect('/contact');
+    }
+    // Put your secret key here.
+    var secretKey = process.env.RECAPTCHA_PRIVATE_KEY;
+    // req.connection.remoteAddress will provide IP address of connected user.
+    var verificationUrl = "https://www.google.com/recaptcha/api/siteverify?secret=" + secretKey + "&response=" + req.body['g-recaptcha-response'] + "&remoteip=" + req.connection.remoteAddress;
+    // Hitting GET request to the URL, Google will respond with success or error scenario.
+    request(verificationUrl, function(error,response,body) {
+        body = JSON.parse(body);
+        // Success will be true or false depending upon captcha validation.
+        if(body.success !== undefined && !body.success) {
+            req.flash('error', "Failed captcha verification");
+            return res.redirect('/contact');
+        }
         var data = {
-            to: 'hrithik7132@gmail.com',
+            to: process.env.EMAIL,
             from: req.body.email,
             subject: req.body.subject,
             text: req.body.content
         }
-        await sgMail.send(data);
-        req.flash('success', 'Message successfully sent to Hrithik')
-        res.redirect('/contact');
-    }catch(e){
-        req.flash('error', e.message);
-        res.redirect('/contact');
-    }
+        sgMail.send(data , (err, json) => {
+            if(err){
+                req.flash('error', err.message);
+                res.redirect('/contact');
+            }
+            req.flash('success', 'Message successfully sent to Hrithik')
+            res.redirect('/contact');
+        });
+    });
 });
 
 
