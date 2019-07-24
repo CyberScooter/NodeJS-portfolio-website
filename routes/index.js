73b19d57d7f517614859dc878cfe7366e66cda2f
Hrithik <hrithik757@gmail.com>
2019-07-24 16:26:58 +0100

    React.js description

--- routes/index.js
+++ routes/index.js
@@ -1,14 +1,14 @@
 const express = require('express'),
-      sgMail = require('@sendgrid/mail')
-      request = require('request');
-      
+    sgMail = require('@sendgrid/mail')
+request = require('request');
+
 sgMail.setApiKey(process.env.SENDGRID_API_KEY);
 
 router = express.Router();
 
 //HOME=============================================================
 
-router.get("/", function(req, res){
+router.get("/", function (req, res) {
     res.render("landing");
 })
 
@@ -24,10 +24,10 @@ router.get("/contact", (req, res) => {
     res.render("contact");
 })
 
-router.post("/contact", (req, res) =>{
+router.post("/contact", (req, res) => {
     // g-recaptcha-response is the key that browser will generate upon form submit.
     // if its blank or null means user has not selected the captcha, so return the error.
-    if(req.body['g-recaptcha-response'] === undefined || req.body['g-recaptcha-response'] === '' || req.body['g-recaptcha-response'] === null) {
+    if (req.body['g-recaptcha-response'] === undefined || req.body['g-recaptcha-response'] === '' || req.body['g-recaptcha-response'] === null) {
         req.flash('error', "Please select captcha");
         return res.redirect('/contact');
     }
@@ -36,10 +36,10 @@ router.post("/contact", (req, res) =>{
     // req.connection.remoteAddress will provide IP address of connected user.
     var verificationUrl = "https://www.google.com/recaptcha/api/siteverify?secret=" + secretKey + "&response=" + req.body['g-recaptcha-response'] + "&remoteip=" + req.connection.remoteAddress;
     // Hitting GET request to the URL, Google will respond with success or error scenario.
-    request(verificationUrl, function(error,response,body) {
+    request(verificationUrl, function (error, response, body) {
         body = JSON.parse(body);
         // Success will be true or false depending upon captcha validation.
-        if(body.success !== undefined && !body.success) {
+        if (body.success !== undefined && !body.success) {
             req.flash('error', "Failed captcha verification");
             return res.redirect('/contact');
         }
@@ -49,8 +49,8 @@ router.post("/contact", (req, res) =>{
             subject: req.body.subject,
             text: req.body.content
         }
-        sgMail.send(data , (err, json) => {
-            if(err){
+        sgMail.send(data, (err, json) => {
+            if (err) {
                 req.flash('error', err.message);
                 res.redirect('/contact');
             }
